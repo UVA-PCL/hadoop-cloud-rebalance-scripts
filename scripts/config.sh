843e08f405ff71aef4b825e13bf76a28e901dbee
cr4bd <cr4bd@virginia.edu>
2019-02-26 15:00:26 -0500

    Rearrange script directory to separate out hadoop configs and adjust scripts to use config.sh for settings

--- scripts/config.sh
+++ scripts/config.sh
@@ -2,6 +2,7 @@ EXPERIMENT_DIR=$(realpath $(dirname $0)/..)
 EXPERIMENT_TEMPLATE_DIR=$EXPERIMENT_DIR/templates
 EXPERIMENT_LOG_DIR=$EXPERIMENT_DIR/log
 EXPERIMENT_SCRIPT_DIR=$EXPERIMENT_DIR/scripts
+EXPERIMENT_HADOOP_CONFIG_DIR=$EXPERIMENT_DIR/hadoop-configs
 EXPERIMENT_SSH="ssh -l zl4dc"
 EXPERIMENT_MACHINES=(shen-51 shen-49 shen-47 shen-44 shen-43 shen-41 shen-39 shen-38 shen-37 shen-35 shen-33 shen-31 shen-29 shen-27 shen-25 shen-24 shen-23 shen-19 shen-18 shen-17 shen-15 shen-14 shen-13 shen-11 shen-9)
 EXPERIMENT_CHORD_SRC=XXX # FIXME
