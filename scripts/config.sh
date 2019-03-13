921586d73bf9d307391040301f196f7bdc54de2e
cr4bd <cr4bd@virginia.edu>
2019-03-12 20:24:29 -0400

    script edits to get them working in simulation env

--- scripts/config.sh
+++ scripts/config.sh
@@ -1,10 +1,25 @@
-EXPERIMENT_DIR=$(realpath $(dirname $0)/..)
+EXPERIMENT_DIR=$(realpath $(dirname ${BASH_SOURCE[0]})/..)
+declare -a EXPERIMENT_DIRS EXPERIMENT_MACHINES EXPERIMENT_IDS
+for i in $(seq 0 3); do
+    EXPERIMENT_IDS[$i]=$i
+    EXPERIMENT_DIRS[$i]=/localtmp/balancing-local/node-$i
+    EXPERIMENT_MACHINES[$i]=localhost
+done
+JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64
+export JAVA_HOME
+EXPERIMENT_HADOOP_TGZ=$EXPERIMENT_DIR/hadoop-2.8.5.tar.gz
+EXPERIMENT_HADOOP_TGZ_DIR=hadoop-2.8.5
+EXPERIMENT_SSH="ssh"
 EXPERIMENT_TEMPLATE_DIR=$EXPERIMENT_DIR/templates
 EXPERIMENT_LOG_DIR=$EXPERIMENT_DIR/log
 EXPERIMENT_SCRIPT_DIR=$EXPERIMENT_DIR/scripts
 EXPERIMENT_HADOOP_CONFIG_DIR=$EXPERIMENT_DIR/hadoop-configs
-EXPERIMENT_SSH="ssh -l zl4dc"
-EXPERIMENT_MACHINES=(shen-51 shen-49 shen-47 shen-44 shen-43 shen-41 shen-39 shen-38 shen-37 shen-35 shen-33 shen-31 shen-29 shen-27 shen-25 shen-24 shen-23 shen-19 shen-18 shen-17 shen-15 shen-14 shen-13 shen-11 shen-9)
 EXPERIMENT_CHORD_SRC=XXX # FIXME
 
-EXPERIMNET_NODE_COUNTS=(10 10 10 10 9 9 9 9 7 7 7 7 4 4 4 4 1 1 1 1)
+EXPERIMENT_NODE_COUNTS=(4 4)
+
+ssh_into_id() {
+    local i=$1
+    shift
+    $EXPERIMENT_SSH ${EXPERIMENT_MACHINES[$i]} ". ${EXPERIMENT_DIRS[$i]-$EXPERIMENT_DIR}/scripts/config.sh; ${@}" 
+}
